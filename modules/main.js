6666caf5279517d8ccbde4955a51f75f513317c4
bormat <bormat2@gmail.com>
2016-10-26 20:42:17 +0200

    batman

--- modules/main.js
+++ modules/main.js
@@ -1,43 +1,57 @@
-mapFuncsToWindow( )
-$( document ).ready(( ) => {
-	if ( window.innerWidth >= 1350 )
-		$( '#fullpage' ).fullpage( )
-})
-$( 'input[name=func]' ).mSelectDBox({
-	"list": [ "x < PI ? PI :  0","x < 0 ? 1+x : 1-x" , "x**3" ]
-});
-$(document.body, '.m-select-d-box__list-item', ( ) => alert( ))
-$( '#generate' ).click( generateGraph )
-// $( '#generate' ).click( )
+mapFuncsToWindow()
 
-$('#fourier-inputs > div:nth-child(1) > div:nth-child(1) > input[type="text"]').focusout(function(){
-	setTimeout(()=>{
-	 switch(this.value){
-		case "x < PI ? PI :  0":
-	 		$('input[name=startPeriod]').val('0')
-	 		$('input[name=endPeriod]').val('2*PI')
-	 		$('input[name=min]').val('0')
-	 		$('input[name=max]').val('12')
-	 		$('input[name=yMin]').val('-2')
-	 		$('input[name=yMax]').val('5')
-	 		break;
-		case "x**3":
-	 		$('input[name=startPeriod]').val('-2')
-	 		$('input[name=endPeriod]').val('2')
-	 		$('input[name=min]').val('0')
-	 		$('input[name=max]').val('12')
-	 		$('input[name=yMin]').val('-10')
-	 		$('input[name=yMax]').val('10')
-	 		break;
-		case "x < 0 ? 1+x : 1-x":
-	 		$('input[name=startPeriod]').val('-2')
-	 		$('input[name=endPeriod]').val('2')
-	 		$('input[name=min]').val('-4')
-	 		$('input[name=max]').val('8')
-	 		$('input[name=yMin]').val('-1.5')
-	 		$('input[name=yMax]').val('1.5')
-	 	default: break;
-	 }
-	},200)
-})
-//#energie-err > b
+const generateGraph = () => {
+	var plot;
+	$(function () {
+	    plot = $.plot($("#placeholder"),
+	                     generateParamForGraphic(), generateParamForAxe());
+	    var legends = $("#placeholder .legendLabel");
+	    legends.each(function () {
+	        // fix the widths so they don't jump around
+	        $(this).css('width', $(this).width());
+	    });
+
+	    var updateLegendTimeout = null;
+	    var latestPosition = null;
+
+	    function updateLegend() {
+	        updateLegendTimeout = null;
+
+	        var pos = latestPosition;
+
+	        var axes = plot.getAxes();
+	        if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+	            pos.y < axes.yaxis.min || pos.y > axes.yaxis.max)
+	            return;
+
+	        var i, j, dataset = plot.getData();
+	        for (i = 0; i < dataset.length; ++i) {
+	            var series = dataset[i];
+
+	            // find the nearest points, x-wise
+	            for (j = 0; j < series.data.length; ++j)
+	                if (series.data[j][0] > pos.x)
+	                    break;
+
+	            // now interpolate
+	            var y, p1 = series.data[j - 1], p2 = series.data[j];
+	            if (p1 == null)
+	                y = p2[1];
+	            else if (p2 == null)
+	                y = p1[1];
+	            else
+	                y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+
+	            legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+	        }
+	    }
+
+	    $("#placeholder").bind("plothover",  function (event, pos, item) {
+	        latestPosition = pos;
+	        if (!updateLegendTimeout)
+	            updateLegendTimeout = setTimeout(updateLegend, 50);
+	    });
+	});
+}
+
+$('#generate').click(generateGraph)
